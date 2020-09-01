2824e1dcc456dbeb97bd968333386f32f5daf4f8
robertloy <robert.loy@gcu.edu>
2020-09-01 08:56:40 -0700

    initial commit

---
+++ src/cst105/week3/battle/Arena.java
@@ -0,0 +1,46 @@
+package cst105.week3.battle;
+
+public class Arena {
+
+	// This is static so no need to create object before calling it
+	// To access this method/function from another class, use
+	// Arena.titleScreen()
+	public static void titleScreen() {
+		
+		System.out.println("=======================");
+		System.out.println("WELCOME TO TODAY'S MAIN EVENT!");
+		System.out.println("=======================");
+		System.out.println("");  // print empty line
+	}
+	
+	// This is static so no need to create object before calling it
+	// To access this method/function from another class, use
+	// Arena.startScreen()
+	public static void startScreen() {
+		
+		System.out.println("THE BATTLE BEGINS IN ... ");
+		
+		// Count backwards from 5 to 0
+		for (int x = 5; x >=  0; x --) {
+			
+			// Stay on  the same line for the next iteration
+			System.out.print(x + " . . . ");
+		}
+		
+		// end the countdown line and move the cursor to next line
+		System.out.println(" BATTLE!");
+		System.out.println("");   // print empty line
+	}
+	
+	// This is static so no need to create object before calling it
+	// To access this method/function from another class, use
+	// Arena.endScreen()
+	public static void endScreen() {
+		
+		System.out.println("=======================");
+		System.out.println("We have our WINNER!");
+		System.out.println("=======================");
+		System.out.println(""); // print empty line
+		
+	}
+}
---
+++ src/cst105/week3/battle/Monster.java
@@ -0,0 +1,8 @@
+package cst105.week3.battle;
+
+public class Monster {
+
+	public String name;
+	public int hp;
+	
+}
